fd1277eac7f4c8c76aebfad65c05fb9a683e64fd
arya <arya.pratamal@ymail.com>
2021-05-09 17:51:59 +0200

    added mobile for homepage

--- client/src/component/HomePage/HomePage.tsx
+++ client/src/component/HomePage/HomePage.tsx
@@ -1,26 +1,32 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import styled from 'styled-components'
 import axios from 'axios';
 import Loader from "react-loader-spinner";
 import ProductCard from '../Card/ProductCard'
 import Body from '../Body/Body'
 import images from '../../img'
+import { screenBreakpoints, onDevice } from '../constant/theme'
+import useWindowSize from '../constant/useWindowSize'
 
 //Example data
 import dataExample from '../constant/dataExample'
 
-const BodyContainer = styled.div`
-  display: flex;
-  flex-direction:column;
-  width:100%;
-  margin: 0 20px;
-`
-const SortBar = styled.div`
-  display:flex;
-  height: 50px;
-  width:100%;
-  margin-top:0;
-`
+const { hero1, hero2, hero3, hero_mobile1, hero_mobile2 } = images;
+const heroImages = [hero1, hero2, hero3];
+const heroImagesMobile = [hero_mobile1, hero_mobile2];
+
+// const BodyContainer = styled.div`
+//   display: flex;
+//   flex-direction:column;
+//   width:100%;
+//   margin: 0 20px;
+// `
+// const SortBar = styled.div`
+//   display:flex;
+//   height: 50px;
+//   width:100%;
+//   margin-top:0;
+// `
 
 const ProductBlock = styled.div`
   display:flex;
@@ -30,26 +36,63 @@ const ProductBlock = styled.div`
   justify-content:center;
 `
 
-const HeroContainer = styled.div`
-  margin: 0 auto;
+interface IHeroContainer {
+  imageLink: any
+}
+
+const HeroContainer = styled.div<IHeroContainer>`
+  background-image: url(${(p) => p.imageLink});
+  width:100vw;
+  height: 90vh;
+  display:flex;
+  background-position:center;
+  background-repeat:no-repeat;
+  background-size: contain;
+  
+  ${onDevice.mobileL}{
+    height: 40vh;
+  }
+
+  ${onDevice.mobileS}{
+    height: 100vh;
+  }
+
 `
 
 const HomePage: FC = () => {
+
+  const [heroImageIndex, setHeroImageIndex] = useState(Number);
+  const { width } = useWindowSize()
 
   const products = dataExample.map((data, index) => {
     return (<ProductCard key={`product-card:${index}`} product={data} />)
   })
 
-  const { hero1 } = images
+  const limit = width > screenBreakpoints.mobileS ? 2 : 1;
+
+  let counter = 0;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (counter > limit) {
+        counter = 0;
+      }
+      setHeroImageIndex(counter);
+      counter++;
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const heroImageUrl = width > screenBreakpoints.mobileS ? heroImages[heroImageIndex] : heroImagesMobile[heroImageIndex];
 
   return (
     <Body>
 
       {/* <BodyContainer> */}
       {/* <SortBar> This is for sorting </SortBar> */}
-      <HeroContainer>
-        <img src={hero1} alt={'hero1'} />
-      </HeroContainer>
+      {/* <Slideshow /> */}
+      {/* <HeroImage /> */}
+      <HeroContainer imageLink={heroImageUrl} />
       <ProductBlock>
         {products}
       </ProductBlock>
--- client/src/img/index.tsx
+++ client/src/img/index.tsx
@@ -1,10 +1,10 @@
 import hero1 from './hero_1.png'
 import hero2 from './hero_2.png'
 import hero3 from './hero_3.png'
-import hero4 from './hero_4.png'
-import hero5 from './hero_5.png'
+import hero_mobile1 from './mobile_hero1.png'
+import hero_mobile2 from './mobile_hero2.png'
 
 
-const images = { hero1, hero2, hero3, hero4, hero5 }
+const images = { hero1, hero2, hero3, hero_mobile1, hero_mobile2 }
 
 export default images
