6cd26387227c12459d58432df0e48221b32c2f83
arya <arya.pratamal@ymail.com>
2021-05-17 12:29:32 +0200

    test backend heroku

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,15 @@ import loggerMiddleware from './logger/loggerMiddleWare';
 const port = process.env.PORT || config.port;
 const app = express();
 
-// // Configure Express to use EJS
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+// Configure Express to use EJS
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// // define a route handler for the default Admin home page
-// app.get('/admin', (req, res) => {
-//   // render the index template
-//   res.render('index');
-// });
+// define a route handler for the default Admin home page
+app.get('/', (req, res) => {
+  // render the index template
+  res.render('index');
+});
 
 // Front-end
 app.use(express.static('client'));
