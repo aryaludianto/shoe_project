828ad11c1ce64aea488d0a5d2167492fe2cadf85
arya <arya.pratamal@ymail.com>
2021-05-17 10:06:22 +0200

    removed things

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,6 @@ dotenv.config();
 
 export default {
   appName: process.env.APP_NAME,
-  port: process.env.SERVER_PORT,
+  port: process.env.PORT || process.env.SERVER_PORT,
   monggoUri: process.env.MONGGO_DB_URI,
 };
