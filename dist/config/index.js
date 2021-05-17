2a18c58938b1d8c5517918d43e3eaa5b98c56422
arya <arya.pratamal@ymail.com>
2021-05-17 11:53:56 +0200

    changed port

--- dist/config/index.js
+++ dist/config/index.js
@@ -2,14 +2,14 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var process_1 = __importDefault(require("process"));
 dotenv_1.default.config();
+// process.env.SERVER_PORT ??
 exports.default = {
     appName: process_1.default.env.APP_NAME,
-    port: (_a = process_1.default.env.SERVER_PORT) !== null && _a !== void 0 ? _a : process_1.default.env.PORT,
+    port: process_1.default.env.PORT,
     monggoUri: process_1.default.env.MONGGO_DB_URI,
 };
 //# sourceMappingURL=index.js.map
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 import process from 'process';
 
 dotenv.config();
-
+// process.env.SERVER_PORT ??
 export default {
   appName: process.env.APP_NAME,
-  port: process.env.SERVER_PORT ?? process.env.PORT,
+  port: process.env.PORT,
   monggoUri: process.env.MONGGO_DB_URI,
 };
