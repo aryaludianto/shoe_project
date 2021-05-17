b65f5a5e89f4a37b4acb68efc9c441654ef9e67a
arya <arya.pratamal@ymail.com>
2021-05-17 10:13:56 +0200

    added path to client

--- dist/index.js
+++ dist/index.js
@@ -23,9 +23,9 @@ app.get('/admin', function (req, res) {
 if (process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express_1.default.static('client/build'));
-    // app.get('*', (req, res) => {
-    //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    // })
+    app.get('*', function (req, res) {
+        res.sendFile(path_1.default.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 // Activity Logger
 app.use(loggerMiddleWare_1.default);
